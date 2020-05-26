2067bd110cc7119e0ad83c01d0158f70ec6831a8
Jihee Hwang <alarmringing@gmail.com>
2020-05-27 02:40:21 +0900

    Refactored Siah Yong's code into RoverPlayerController and InteractionTestObject Added transformation state text for transformation

---
+++ Assets/Scripts/RoverObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverObject : MonoBehaviour
+{
+    public bool isLifeInteractable = true;
+    public string scanMessage = "It doesn't seem to be life.";
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        RoverPlayerController controller = collision.gameObject.GetComponent<RoverPlayerController>();
+        if (!controller) return;
+        controller.SetInteractableTarget(this);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        RoverPlayerController controller = collision.gameObject.GetComponent<RoverPlayerController>();
+        if (!controller) return;
+        controller.SetInteractableTarget(null);
+    }
+}
--- Assets/Scripts/RoverPlayerController.cs
+++ Assets/Scripts/RoverPlayerController.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoverPlayerController : MonoBehaviour
 {
+    // Transformation
     public Collider2D waterArea;
     public Collider2D landArea;
-    
+    public Text roverState;
+
+    public GameObject ScanMessage; //added to display scan results
+    public bool scanMode; //added to manage scan dialogue
+    public bool socket; //added to check if socket has been activated
+    private Text scanText;
+    private RoverObject interactableTarget;
+
+    // Appearance
     IsometricCharacterRenderer isoRenderer;
 
+    // Movement
     private Rigidbody2D rbody;
     private float movementSpeed = 1f;
 
@@ -16,19 +27,54 @@ public class RoverPlayerController : MonoBehaviour
     {
         rbody = GetComponent<Rigidbody2D>();
         isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+
+        scanText = ScanMessage.GetComponentInChildren<Text>();
+        scanText.text = ""; //line added for scanning purposes
+        scanMode = false; //line added for scanning purposes
+        ScanMessage.SetActive(scanMode);
     }
 
 
     // Update is called once per frame
+    private void Update()
+    {
+        ScanCheck();
+
+        TransformController();
+        
+    }
+
     void FixedUpdate()
     {
-        TransformController();
+        if (scanMode) return;
         MovementController();
-       
     }
 
     public void ChangeMovementSpeed(float newSpeed) { movementSpeed = newSpeed; }
 
+    public void SetInteractableTarget(RoverObject target) { interactableTarget = target; }
+
+    private void ScanCheck()
+    {
+        // Scan key is pressed AND rover is close to some interactable object
+        if (Input.GetKeyDown("h") && interactableTarget != null)
+        {
+            Debug.Log("GetKeyDown is h");
+            if (scanMode == false) 
+            {
+                scanMode = true;
+                scanText.text = interactableTarget.scanMessage;
+            }
+            else
+            {
+                Debug.Log("Now disabling scanMode");
+                scanMode = false;
+                scanText.text = "";
+            }
+            ScanMessage.SetActive(scanMode);
+        }
+    }
+    
     private void MovementController()
     {
         // Handle Movement
@@ -60,6 +106,7 @@ public class RoverPlayerController : MonoBehaviour
     private void TransformMode(string goal)
     {
         gameObject.tag = goal;
+        roverState.text = goal;
         Debug.Log("Transforming to: " + goal);
     }
 }
